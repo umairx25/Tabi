a2648a1f37654c1e2080b6c1c6eed9ecdf6cd397
umairx25 <umairarhambd@gmail.com>
2025-12-19 15:14:24 -0500

    Added remote backend url instead of localhost

--- extension/popup.js
+++ extension/popup.js
@@ -6,8 +6,7 @@ and carrying out in-browser actions (like modifying/creating/switching tabs).
 
 const input = document.getElementById("search-input");
 const results = document.getElementById("results");
-const BACKEND_URL = "http://127.0.0.1:8000";
-// const BACKEND_URL = "https://tabi-api.onrender.com";
+const BACKEND_URL = "https://tabi-api-10z9.onrender.com";
 
 let search_suggestion;
 
